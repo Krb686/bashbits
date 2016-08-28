897164d6864d36879b453493db53bd8c82ee80c7
kevin <krb686@gmail.com>
2016-08-27 18:59:43 -0700

    bash analyzer finding comments and strings

--- bash_analyzer.sh
+++ bash_analyzer.sh
@@ -4,11 +4,17 @@
 LOG_FLAG=1
 ERROR_FLAG=1
 DEBUG_FLAG=1
+
+DUMP_COMMENTS=0
+DUMP_STRINGS=1
 # ------------
-BUILD_STRING=1
+BUILD_STR=1
 LINENUM=1
 STATE="init"
 STR=
+STRING=
+COMMENT=
+STR_ESCAPE="off"
 
 # Exit codes
 EXIT_NO_BASH_FUNCS=1
@@ -39,6 +45,8 @@ function exit {
 function main {
     trap exit EXIT
 
+    local file="${1:?'Need to specify file!'}"
+
     parse_loop "$1"
 }
 
@@ -47,40 +55,80 @@ function main {
 # ====================================================== #
 function parse_loop {
     # Main loop
-    while read -r -n1 char; do
 
-        if [[ $char == $'\n' ]]; then
-            printf "yes, char is newline\n"
+#:<<'#b'
+    IFS=
+    STR=
+    while read -rN1 char; do
+        #printf "%s" "${char}"
+
+        # Always turn off after a single char
+        if [[ "$STR_ESCAPE" == "trigger" ]]; then
+            STR_ESCAPE="on"
+        elif [[ "$STR_ESCAPE" == "on" ]]; then
+            STR_ESCAPE="off"
         fi
 
         case $char in
             "#")
-                [[ "$STATE" == "norm" ]] && { STATE="comment"; };; #printf "Now inside comment at line: $LINENUM!\n"; };;
-            "")
-                [[ "$STATE" == "comment" ]] && { STATE="norm"; } #printf "Now inside norm!\n"; }
-                LINENUM=$(($LINENUM+1))
-                printf "Ater inc, linenum = $LINENUM\n";;
-        esac
-
-        [ $BUILD_STRING -eq 1 ] && STR+=$char
-
-  #      printf "%s\n" "$STR"
-        case $STR in
-            "#!/bin/bash")
-                STR=
-                printf "Got shebang!\n"
-                STATE="norm";;
-            "function")
-                printf "Found 'function'\n"
+                [[ "$STATE" == "norm" ]] && { STATE="comment"; } #printf "Now inside comment at line: $LINENUM!\n"; };;
                 ;;
+            "\\")
+                if [[ "$STATE" == "str_double" ]] && [[ "$STR_ESCAPE" == "off" ]]; then
+                    #printf "%s\n" "Turning STR_ESCAPE to trigger at lineno $LINENUM"
+                    STR_ESCAPE="trigger"
+                fi
+                ;;
+            "\"")
+                if [[ "$STATE" == "norm" ]]; then
+                    #printf "%s\n" "Entering state - str_double - at lineno $LINENUM"
+                    STATE="str_double"
+                elif [[ "$STATE" == "str_double" ]] && [[ "$STR_ESCAPE" == "off" ]]; then
+                    STATE="norm"
+                    [[ "$DUMP_STRINGS" -eq 1 ]] && printf "%s\n" "STRING = $STRING, at lineno $LINENUM"
+                     STRING=
+                fi
+                ;;
+            $'\n')
+                if [[ "$STATE" == "comment" ]]; then
+                    STATE="norm"
+                    [[ "$DUMP_COMMENTS" -eq 1 ]] && printf "%s\n" "COMMENT = $COMMENT, at lineno $LINENUM"
+                    COMMENT=
+                fi
+                LINENUM=$(($LINENUM+1))
+
+                ;;
+ #printf "Now inside norm!\n"; }
             "")
-                STR=
-                printf "Got newline!\n"
+                                #LINENUM=$(($LINENUM+1))
+                #printf "Ater inc, linenum = $LINENUM\n";;
         esac
+
+        
+        [[ $BUILD_STR -eq 1 ]] && STR+="$char" || STR=
+        [[ "$STATE" == "comment" ]] && COMMENT+="$char"
+        [[ "$STATE" == "str_double" ]] && STRING+="$char"
+
+        case "$STR" in
+            "#!/bin/bash")
+                printf "%s\n" "got shebang!"
+                STATE="norm"
+                STR=
+                ;;
+        esac
+
     done <<< "$(<${1:?'No file!'})"
+#b
 }
 
-main "$1"
+main $*
+
+
+
+
+
+
+
 
 
 
